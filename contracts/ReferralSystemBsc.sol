21cadd01a053f2771a8295925d97bd6712fa9832
an-ivannikov <info@ivannikov.pro>
2023-02-09 02:51:45 +0400

    up

--- contracts/ReferralSystemBsc.sol
+++ contracts/ReferralSystemBsc.sol
@@ -10,18 +10,62 @@ contract ReferralSystemBsc is Ownable, Pausable {
     using Address for address;
     using BinaryTreeLib for BinaryTreeLib.Tree;
 
+    uint256[] public prices = [
+        0,
+        0.2 ether,
+        0.3 ether,
+        0.5 ether,
+        1 ether,
+        2 ether,
+        3 ether,
+        5 ether,
+        10 ether,
+        18 ether,
+        30 ether,
+        50 ether,
+        100 ether,
+        180 ether,
+        300 ether,
+        500 ether
+    ];
+    uint256[] public series = [
+        0,
+        3000,
+        2800,
+        2300,
+        2000,
+        1700,
+        1500,
+        1000,
+        550,
+        300,
+        200,
+        100,
+        50,
+        25,
+        20,
+        10
+    ];
+
     BinaryTreeLib.Tree private tree;
 
     constructor() public {
         //
     }
 
-    // pause
     function pause() external onlyOwner {
         _pause();
     }
 
     function unpause() external onlyOwner {
         _unpause();
+    }
+
+    function reduceQuantity(uint256 level, uint256 quantity)
+        external
+        onlyOwner
+    {
+        require(series[level] >= quantity, "Incorrect quantity");
+        series[level] -= quantity;
     }
 }
--- contracts/ReferralSystemPolygon.sol
+++ contracts/ReferralSystemPolygon.sol
@@ -10,18 +10,65 @@ contract ReferralSystemPolygon is Ownable, Pausable {
     using Address for address;
     using ReferralTreeLib for ReferralTreeLib.Tree;
 
+    // @TODO
+    uint256[] public prices = [
+        0,
+        0.001 ether,
+        0.002 ether,
+        0.003 ether,
+        0.004 ether,
+        0.005 ether,
+        0.006 ether,
+        0.007 ether,
+        0.008 ether,
+        0.009 ether,
+        0.010 ether,
+        0.011 ether,
+        0.012 ether,
+        0.013 ether,
+        0.014 ether,
+        0.015 ether,
+        0.016 ether
+    ];
+    uint256[] public series = [
+        0,
+        3000,
+        2500,
+        2200,
+        1800,
+        1500,
+        1300,
+        1100,
+        800,
+        500,
+        300,
+        200,
+        150,
+        100,
+        70,
+        25,
+        10
+    ];
+
     ReferralTreeLib.Tree private tree;
 
     constructor() public {
         //
     }
 
-    // pause
     function pause() external onlyOwner {
         _pause();
     }
 
     function unpause() external onlyOwner {
         _unpause();
+    }
+
+    function reduceQuantity(uint256 level, uint256 quantity)
+        external
+        onlyOwner
+    {
+        require(series[level] >= quantity, "Incorrect quantity");
+        series[level] -= quantity;
     }
 }
